64dee924779359f096b75afa6b9e3d080a8516d6
Alexanderr23 <ar27109@gmail.com>
2022-06-09 22:05:25 -0400

    ReviewClass

---
+++ src/gitBatch13/HW.java
@@ -0,0 +1,83 @@
+package gitBatch13;
+
+public class HW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	
+	
+
+		// TODO Auto-generated method stub
+
+	
+		//String str=new String("java clas is soon");
+		//System.out.println(str.trim());
+
+	  //String str="today will be  a good day";
+	  //System.out.println(str.replaceAll("[ ]",""));
+
+//String str="aBCdef123%##!";
+//System.out.println(str.replaceAll("[^a-zA-Z0-9]","").length());
+	
+	
+	//int count=0;
+	//String a="Is it saturday?Is it raining?Do we have java class? ";
+   //  for(int i=0;i<a.length();i++) {
+//	if(a.charAt(i)=='?') {
+	//	count++;
+	
+	
+	
+//String text="backwords";
+//String[]words=text.split(" ");
+//String reversewords="";
+
+//for(String word:words) {
+	//String reverse="";
+	//for(int i =word.length()-1;i>=0;i--) {
+	//	reverse+=word.charAt(i);
+	 
+//reversewords+=reverse+" ";
+
+
+
+//}
+
+//System.out.println(reversewords);	
+	
+	
+	
+	String a="LOL";
+	String b="lol";
+	a=a+b;
+	b=a.substring(0,a.length()-b.length());
+	a=a.substring(b.length());	
+	
+	System.out.println("a is now " +a+ " b is now " +b);
+	
+	//}
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	//System.out.println(count);
+//}
+
