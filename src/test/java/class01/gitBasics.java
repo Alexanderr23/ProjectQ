1fea5b7a4f70fce046193373d12a8cbc65b11ce0
Alexanderr23 <ar27109@gmail.com>
2022-08-24 20:58:29 -0400

    tempreture check1

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -3,7 +3,7 @@ package class01;
 public class gitBasics {
     public static void main(String[] args) {
      //write a code tha checks the temp of a website then prints out is a beatuiful day or not
-        System.out.println("today is a beautiful day");
+        System.out.println("today is  not a beautiful day");
     }
 
 
