fd0779e720a79cb2590f65b92439a6484488654a
Alexanderr23 <ar27109@gmail.com>
2022-08-24 20:03:12 -0400

    tempreture check

---
+++ src/gitBatch13/Array1.java
@@ -0,0 +1,27 @@
+package gitBatch13;
+
+public class Array1 {
+
+	
+
+	String word;
+
+	
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	
+	}
+
+	StringBuilder rev=new StringBuilder(word);
+	rev.word="alex";
+
+
+
+
+}
